bb0400a5ac312132e60877981bd3ee0f56552820
Khalal Walker <khalal.walker@gmail.com>
2019-07-22 17:37:23 -0700

    update headers

--- Node/infoSite/server.js
+++ Node/infoSite/server.js
@@ -4,7 +4,7 @@ const fs = require("fs");
 function displayPageContent(fileName, response, statusCode) {
     fs.readFile(fileName, (err, data) => {
         if (err) throw err;
-        response.writeHead(200, {'Content-Type': 'text/html'});
+        response.writeHead(statusCode, {'Content-Type': 'text/html'});
         response.write(data);
         response.end();
     });
