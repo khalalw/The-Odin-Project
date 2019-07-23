409e26c8186ea1b5bbb46ea5538b44dcf3b2f846
Khalal Walker <khalal.walker@gmail.com>
2019-07-22 19:22:29 -0700

    update formatting

--- Node/infoSite/server.ts
+++ Node/infoSite/server.ts
@@ -1,32 +1,31 @@
-import http = require("http");
-import fs = require("fs");
+import http = require('http');
+import fs = require('fs');
 
 function displayPageContent(fileName: string, response: http.ServerResponse, statusCode: number) {
-    fs.readFile(fileName, (err, data) => {
-        if (err) throw err;
-        response.writeHead(statusCode, {'Content-Type': 'text/html'});
-        response.write(data);
-        response.end();
-    });
+  fs.readFile(fileName, (err, data) => {
+    if (err) throw err;
+    response.writeHead(statusCode, { 'Content-Type': 'text/html' });
+    response.write(data);
+    response.end();
+  });
 }
-
 
 http
   .createServer((req, res) => {
     const path = req.url;
 
     switch (path) {
-        case '/':
-            displayPageContent('index.html', res, 200);
-            break;
-        case '/contact-me':
-            displayPageContent('contact-me.html', res, 200);
-            break;
-        case '/about':
-            displayPageContent('about.html', res, 200);
-            break;
-        default:
-            displayPageContent('404.html', res, 404);
+      case '/':
+        displayPageContent('index.html', res, 200);
+        break;
+      case '/contact-me':
+        displayPageContent('contact-me.html', res, 200);
+        break;
+      case '/about':
+        displayPageContent('about.html', res, 200);
+        break;
+      default:
+        displayPageContent('404.html', res, 404);
     }
   })
   .listen(4000);
